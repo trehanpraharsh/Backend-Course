db25e6ad69254b37da7f36308ebac1d4a8eb1ce9
Praharsh Trehan <trehanpraharsh@gmail.com>
2024-02-07 23:48:19 +0530

    7th Feb, Checkout

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,2 +1,25 @@
-const hello = "Hello World";
-console.log(hello);
+const http = require('http')
+const url = require('url')
+const PORT = 8000;
+
+const server = http.createServer(function(req, res){
+    //* Headers needs to be sent before sending the message or content.
+    
+    if(req.url === '/overview'){
+        res.end("<h2>Hello, this is Overview</h2>")
+    } else if(req.url === '/'){
+        res.end("<h1>Hello, this is Homepage</h1>")
+    } else if(req.url === '/product'){
+        res.end("<h2>Hello, this is Product</h2>")
+    } else {
+        res.writeHead(404, {
+            'content-type': 'text/html',
+            'my-own-header': 'hello-bhai'
+        })
+        res.end("<h1>Error! Page not found :(</h1>")
+    }
+})
+
+server.listen(PORT, 'localhost', function(){
+    console.log(`Listening to requests on port : ${PORT}`);
+})
